a0369d7fafe30804c3d72e6acf1ac1169cec3865
vitogit <vitogit@users.noreply.github.com>
2016-09-05 12:15:17 -0300

    Add socket.io client. It´s working

--- app/assets/js/main.js
+++ app/assets/js/main.js
@@ -11,4 +11,18 @@ import '../js/lib/linkify'
 import '../js/lib/linkify-html'
 
 riot.messageStore = new MessageStore()
-riot.mount('*')
+
+var SERVER_URI = 'http://localhost:8888';
+var io = require("socket.io-client");
+
+
+
+//If connected to server then add global access to riot.socket and mount tags
+document.addEventListener('DOMContentLoaded', function() {
+  if (typeof io === 'undefined') {
+    alert('Unable to connect to ' + SERVER_URI);
+  } else {
+    riot.socket = io.connect(SERVER_URI);
+    riot.mount('*')
+  }
+});
